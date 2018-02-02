a0bdde31c0698381ebd47df63ca7ecc8bdbdebf9
Daniel <dgrimes@levelsetsolutions.com>
2018-02-02 12:17:45 -0500

    cards added, all crud operations working in most basic form

--- aspirations/app/controllers/cards_controller.rb
+++ aspirations/app/controllers/cards_controller.rb
@@ -1,10 +1,43 @@
 class CardsController < ApplicationController
   def index
+  	@cards = Card.all
   end
 
   def new
+  	@card = Card.new
   end
 
-  def view
+  def create
+  	@card = Card.new(card_params)
+
+  	@card.save
+  	redirect_to cards_url
   end
+
+  def show
+  	@card = Card.find(params[:id])
+  end
+
+  def edit
+  	@card = Card.find(params[:id])
+  end
+
+  def update
+  	@card = Card.find(params[:id])
+
+  	@card.update(card_params)
+  	redirect_to cards_url
+  end
+
+  def destroy
+  	@card = Card.find(params[:id])
+
+  	@card.destroy
+  	redirect_to cards_url
+  end
+
+  private
+  		def card_params
+  			params.require(:card).permit(:title, :text)
+  		end
 end
--- aspirations/config/routes.rb
+++ aspirations/config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'cards/index'
+	resources :cards
 
-  get 'cards/new'
-
-  get 'cards/view'
-
-	root 'home#index'
-  get 'home/index'
-
+	root 'cards#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
