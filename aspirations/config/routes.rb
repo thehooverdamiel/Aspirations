4fb7cd3eb51e0b470e626fbb8c3e503c82fb4b37
Daniel <dgrimes@levelsetsolutions.com>
2018-02-02 11:04:24 -0500

    added cards controller, model, commiting to make sure master is up to date locally

--- aspirations/config/routes.rb
+++ aspirations/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'cards/index'
+
+  get 'cards/new'
+
+  get 'cards/view'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
